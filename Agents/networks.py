94d62801614a42e8e48474fd07b1f815b8c5c201
Alex Schutz <74842472+alex-schutz@users.noreply.github.com>
2024-10-25 12:56:16 +0100

    Apply Black formatting

--- Agents/networks.py
+++ Agents/networks.py
@@ -4,21 +4,22 @@ import flax
 import flax.linen as nn
 from typing import Sequence
 
+
 # Fully connected MLP
 class QNetwork(nn.Module):
     hidden_dims: Sequence[int]
-    num_actions:int
+    num_actions: int
 
     @nn.compact
     def __call__(self, x: jnp.ndarray):
-        #If only inputting 1 sample at a time, broadcast so leading dimension size is 1
-        #Flatten the input to (batch size, n_nodes*n_nodes*3)
+        # If only inputting 1 sample at a time, broadcast so leading dimension size is 1
+        # Flatten the input to (batch size, n_nodes*n_nodes*3)
         x = x.reshape((x.shape[0], -1))
         for i, hidden_dim in enumerate(self.hidden_dims):
             # nn.Dense is only applied over the last dimension of the input
             x = nn.Dense(hidden_dim)(x)
             x = nn.relu(x)
         x = nn.Dense(self.num_actions)(x)
-        #Output has shape (batch_size, num_actions)
-        #Maybe add softmax here
+        # Output has shape (batch_size, num_actions)
+        # Maybe add softmax here
         return x
--- Unit_Tests/test_networks.py
+++ Unit_Tests/test_networks.py
@@ -1,16 +1,16 @@
 import jax
 import jax.numpy as jnp
 import sys
-sys.path.append('..')
+
+sys.path.append("..")
 from Agents.networks import QNetwork
 
 if __name__ == "__main__":
     key = jax.random.PRNGKey(0)
-    #Batch size, number of nodes, number of nodes, 3
-    x = jax.random.uniform(key, (10,5,5,3))
-    model = QNetwork([128,64,32,16],6)
+    # Batch size, number of nodes, number of nodes, 3
+    x = jax.random.uniform(key, (10, 5, 5, 3))
+    model = QNetwork([128, 64, 32, 16], 6)
     params = model.init(key, x)
     print(jax.tree_map(lambda x: x.shape, params))
     y = model.apply(params, x)
     print(jax.tree_map(lambda x: x.shape, y))
-
