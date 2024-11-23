c8c78ffcfd01f8641965a95f275fba8a74380865
lamlamnt <lam.lam@pmb.ox.ac.uk>
2024-11-23 23:52:31 +0000

    fixed bash script

--- run_experiment_node_10.sh
+++ run_experiment_node_10.sh
@@ -3,8 +3,10 @@
 random_seed_for_training=(30 70 71 72 73 74 75)
 prop_stoch=(0.4 0.8 0.3 0.5 0.7 0.8 0.9)
 
-for run in "${learning_rates[@]}"
+for i in "${random_seed_for_training[@]}"
 do
-    log_dir = "node10/random_seed_${run}_prop_stoch_${run}"
-    python main_ppo.py --n_node 10 --time_steps 100000 --log_directory logs/ --random_seed_for_training $run --prop_stoch $run --wandb_mode online --wandb_project_name node10_different_graphs
+    seed="${random_seed_for_training[$i]}"
+    stoch="${prop_stoch[$i]}"
+    log_dir="node10/random_seed_${seed}_prop_stoch_${stoch}"
+    python main_ppo.py --n_node 10 --time_steps 100000 --log_directory logs/ --random_seed_for_training "$seed" --prop_stoch "$stoch" --wandb_mode online --wandb_project_name node10_different_graphs
 done
