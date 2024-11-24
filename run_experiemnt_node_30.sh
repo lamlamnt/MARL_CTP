ae46c9e83636743f2d964296caf374591f2b8bcf
lamlamnt <lam.lam@pmb.ox.ac.uk>
2024-11-24 12:55:51 +0000

    Add super large bash script

--- run_experiemnt_node_30.sh
+++ run_experiemnt_node_30.sh
@@ -1,39 +1,3 @@
 #!/bin/bash
 
-learning_rates=("0.001" "0.00025" "0.0001")
-anneal_lr=("True" "False")
-prop_stoch=(0.4 0.8)
-#anneal learning rate and ent_coeff
-clip_range=("0.1" "0.3")
-ent_coef=("0.01" "0.03")
-num_steps_before_update=("128" "256" "512")
-num_minibatches=("4" "6" "8" "10")
-gae_lambdas=("0.93" "0.95" "0.97" "0.99")
-random_seed_for_training=("0" "1") #check the graph in advance 
-
-for lr in "${learning_rates[@]}"
-do
-  for cr in "${clip_range[@]}"
-  do
-    for ec in "${ent_coef[@]}"
-    do
-      for nsbu in "${num_steps_before_update[@]}"
-      do
-        for nmb in "${num_minibatches[@]}"
-        do
-          for na in "${network_activation[@]}"
-          do
-            for nt in "${network_type[@]}"
-            do
-              log_dir="lr_${lr}_cr_${cr}_ec_${ec}_nsbu_${nsbu}_nmb_${nmb}_na_${na}_nt_${nt}"
-              echo "Running experiment with lr=$lr, clip_range=$cr, ent_coef=$ec, num_steps_before_update=$nsbu, num_mini_batch=$nmb, network_activation=$na, network_type=$nt"
-              python main_ppo.py --n_node 30 --time_steps 2000000 --log_directory $log_dir --learning_rate $lr --clip_range $cr --ent_coef $ec --num_steps_before_update $nsbu --num_minibatches $nmb --network_activation $na --network_type $nt
-            done
-          done
-        done
-      done
-    done
-  done
-done
-
 
