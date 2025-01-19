f60efcc6c6eee1887b98cf5304c6922cfb8452e6
lamlamnt <lam.lam@pmb.ox.ac.uk>
2025-01-19 23:08:09 +0000

    add bash scripts

---
+++ ARC/submission_script.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#SBATCH --clusters=htc
+#SBATCH --gres=gpu:1 --constraint='gpu_mem:32GB'
+#SBATCH --time=00:10:00
+#SBATCH --partition=devel
+#SBATCH --job-name=test-arc
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=lam.lam@pmb.ox.ac.uk
+#SBATCH --constraint="[scratch:weka|scratch:gpfs]"
+
+#Maybe include cpu_per_task
+
+cd $SCRATCH || exit 1
+
+#Copy MARL folder to $SCRATCH
+rsync -av $HOME/pemb6454/MARL_CTP ./
+
+module purge
+module load Anaconda3
+source activate /data/engs-goals/pemb6454/marl_2
+
+#not sure if srun interferes with jax
+#export XLA_PYTHON_CLIENT_PREALLOCATE=true 
+#export XLA_PYTHON_CLIENT_MEM_FRACTION=1
+cd ..
+srun python main_ppo.py --n_node 5 --log_directory "test" --time_steps 100000 --network_type "CNN" --learning_rate 0.001 --num_steps_before_update 1000 --clip_eps 0.1366 --num_update_epochs 6 --division_plateau 5 --ent_coeff_schedule "sigmoid" --vf_coeff 0.128 --ent_coeff 0.174 --deterministic_inference_policy True --graph_identifier "node_5_mixed" --prop_stoch 0.4 --num_stored_graphs 2000 --factor_inference_timesteps 1000 --num_minibatches 1 --optimizer_norm_clip 1.0 --frequency_testing 20 --factor_testing_timesteps 50 
+
+#Copy the log directory folder back to $HOME or $DATA
+rsync -av --exclude=input --exclude=bin ./Logs/folder_name $HOME/pemb6454/MARL_CTP/ARC
--- final_evaluation_10_nodes.sh
+++ final_evaluation_10_nodes.sh
@@ -1,2 +1,17 @@
-#Average over 5 random seeds
+#!/bin/bash
+
+random_seed_for_training=(30 32 34)
+prop_stoch=(0.2 0.4)
+
+for prop in "${prop_stoch[@]}"
+do
+    for seed in "${random_seed_for_training[@]}"
+    do
+        next_seed=$((seed + 1))
+        log_dir="node10_results/prop_${prop}_${seed}"
+        graph_identifier_name="node_10_${prop}_training_14k"
+        CUDA_VISIBLE_DEVICES=0 python main_ppo.py --n_node 10 --log_directory "$log_dir" --time_steps 6000000 --network_type "Densenet_Same" --learning_rate 0.001 --num_steps_before_update 3600 --clip_eps 0.1366 --num_update_epochs 6 --division_plateau 5 --ent_coeff_schedule "sigmoid" --vf_coeff 0.128 --ent_coeff 0.174 --deterministic_inference_policy True --graph_identifier "$graph_identifier_name" --prop_stoch "$prop" --num_stored_graphs 14000 --factor_inference_timesteps 1000 --num_minibatches 1 --optimizer_norm_clip 1.0 --frequency_testing 20 --factor_testing_timesteps 50 --random_seed_for_training "$seed" --random_seed_for_inference "$next_seed" 
+    done
+done
+
 
